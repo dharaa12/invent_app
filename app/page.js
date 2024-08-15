f96ab13c619369993eb93d8508d8ba6ec2110cf9
dharaa12 <dharaabahukhandi@gmail.com>
2024-08-15 17:16:33 -0400

    2nd commit

--- app/page.js
+++ app/page.js
@@ -5,5 +5,9 @@ import {firestore} from '@/firebase'
 import {Box, Typography} from '@mui/material'
 
 export default function Home() {
-  return <Box><Typography>Inventory Management</Typography></Box>
+  return (
+    <Box>
+      <Typography variant="h1" >Inventory Management</Typography>
+    </Box>
+  )
 }
