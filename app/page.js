13ba2058c61063c7757680cfb43af158a00194f5
dharaa12 <dharaabahukhandi@gmail.com>
2024-08-15 17:57:15 -0400

    more changes

--- app/page.js
+++ app/page.js
@@ -21,21 +21,65 @@ export default function Home() {
       })
     })
     setInventory(inventoryList)
+    //console.log(inventoryList)
   }
+
+  //remove items
+  const removeItem = async(item)=>{
+    const docRef = doc(collection(firestore,'inventory'), item)
+    const docSnap = await getDoc(docRef)
+
+    if(docSnap.exists()){
+      const {quantity} = docSnap.data()
+      if (quantity === 1){
+        await deleteDoc(docRef)
+      } else{
+        await setDoc(docRef, {quantity: quantity - 1})
+      }
+    }
+
+    await updateInvetory()
+  }
+
+
+  //add items
+  const addItem = async(item)=>{
+    const docRef = doc(collection(firestore,'inventory'), item)
+    const docSnap = await getDoc(docRef)
+
+    if(docSnap.exists()){
+      const {quantity} = docSnap.data()
+        await setDoc(docRef, {quantity: quantity + 1})
+    }
+    else{
+      await setDoc(docRef, {quantity:1})
+    }
+
+    await updateInvetory()
+    }
+
+    
+  }
+
 
   useEffect(()=>{
     updateInvetory()
   }, [])
 
+  //models
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
   return (
     <Box>
       <Typography variant="h1" >Inventory Management</Typography>
       {inventory.forEach((item)=>{
+        console.log(item)
         return (
-        <>
+        <Box>
           {item.name}
           {item.count}
-          </>)
+          </Box>)
         })
       }
     </Box>
