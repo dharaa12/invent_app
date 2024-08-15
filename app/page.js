ccc3395f23ec19ba8571a806876c9ce0605b0d8f
dharaa12 <dharaabahukhandi@gmail.com>
2024-08-15 17:12:50 -0400

    1st commit

--- app/page.js
+++ app/page.js
@@ -1,5 +1,9 @@
+'use client'
 import Image from "next/image";
+import {useState, useEffect} from 'react'
+import {firestore} from '@/firebase'
+import {Box, Typography} from '@mui/material'
 
 export default function Home() {
-  return <>Hello World. Welcome to project 2.</>
+  return <Box><Typography>Inventory Management</Typography></Box>
 }
